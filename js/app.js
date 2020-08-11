d845ef2576a9635f7d2a0372d62ccdaa1b245960
Menighin <joao.menighin@gmail.com>
2020-08-11 19:55:57 -0300

    Slider and about - in progress

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var carousel = document.querySelector('.main-carousel');
+var flkty = new Flickity( carousel, {
+  cellAlign: 'left',
+  contain: false,
+  autoPlay: true,
+  pageDots: false,
+  wrapAround: true
+});
