24724206b1d95f3137986cd9b46f56eb6e9943ac
Menighin <joao.menighin@gmail.com>
2020-08-15 23:12:43 -0300

    In Progress

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// Scroll to
+const scrollToSection = (e, id) => {
+  e.preventDefault();
+  document.getElementById(id).scrollIntoView({behavior: 'smooth'});
+}
+
+// Toggle snout
+const toggleSnout = () => {
+  const actual = document.querySelector('.snout').style.display;
+  if (actual === 'block') {
+    document.querySelector('.snout').style.display = 'none';
+    document.querySelector('.snout-loading').style.display = 'block';
+  } else {
+    document.querySelector('.snout').style.display = 'block';
+    document.querySelector('.snout-loading').style.display = 'none';
+  }
+}
+
+// Configuring carousel
 var carousel = document.querySelector('.main-carousel');
 var flkty = new Flickity( carousel, {
   cellAlign: 'left',
@@ -6,3 +25,37 @@ var flkty = new Flickity( carousel, {
   pageDots: false,
   wrapAround: true
 });
+
+
+// Configuring download link
+const windowsDownload = 'https://github.com/Menighin/porquinho-digital/releases/download/v0.0.3/Porquinho-Digital-Setup-0.0.3.exe';
+const linuxDownload = 'https://github.com/Menighin/porquinho-digital/releases/download/v0.0.3/porquinho-digital-0.0.3.x86_64.rpm';
+
+const getOS = () => {
+  const userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+  let os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+const downloadLink = document.getElementById('download');
+downloadLink.href = getOS() === 'Linux' ? linuxDownload : windowsDownload;
+
+const downloadPlatform = document.getElementById('download-platform');
+downloadPlatform.classList.add(getOS() === 'Linux' ? 'fa-linux' : 'fa-windows');
